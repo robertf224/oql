26f30f6cc80bf35617eeb75ac14acfe735f1157c
Robert Fidler <robertf224@gmail.com>
2025-04-03 12:30:03 -0400

    repo scaffolding

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    printWidth: 110,
+    singleQuote: false,
+    trailingComma: "es5",
+    tabWidth: 4,
+
+    plugins: ["prettier-plugin-packagejson", "prettier-plugin-tailwindcss"],
+};
